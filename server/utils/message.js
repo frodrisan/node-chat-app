21d0ec1ea7abb8167007260b7a4696a62e07c260
frodriguez <frodriguez@20h.es>
2018-04-21 00:21:36 +0100

    Send messages to only people in same room

--- server/utils/message.js
+++ server/utils/message.js
@@ -11,7 +11,7 @@ let generateMessage = (from, text) => {
 let generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
-    url: `https://www.google.es/maps?q=${latitude},${longitude}`,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
     createdAt: moment().valueOf()
   };
 };
